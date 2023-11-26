7dcd527fc9a40830dde9867c0ea3dd6a437c3225
Maher karoui <maher.karoui@esprit.tn>
2023-11-26 18:19:28 +0100

    User design

---
+++ LocaGest/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  LocaGest
+//
+//  Created by Maher Karoui on 26/11/2023.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}

d7e057c3b0c54b40c0022d7afb83489bba2989f3
El Mer <ELMer@MM11L01.local>
2023-11-30 18:03:20 +0100

    Chat Interface Progress

--- LocaGest/Views/Chat/ConversationMessagesView.swift
+++ LocaGest/Views/Chat/ConversationMessagesView.swift
@@ -14,81 +14,76 @@ struct ConversationMessagesView: View {
     @State private var messageText = ""
     let conversation: Conversation
     var body: some View {
-        ScrollView{
-            VStack{
-                Image(conversation.image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 120, height: 120)
-                    .mask(Circle())
-                    .foregroundColor(Color(.systemGray4))
-                    .padding(.vertical)
-                
-                Text(conversation.members[1])
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                
-                Text("Technicien")
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-                    .padding(.bottom)
-                
-                Text("Choisir une image")
-            
-                PhotosPicker(selection: $viewModel.selectedItem){
-                    if let selectedImage = viewModel.selectedImage {
-                        selectedImage
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 180, height: 180)
-                            .clipShape(Rectangle())
-                    } else {
-                        Image(systemName: "photo.circle.fill")
-                            .resizable()
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(Color(.systemGray4))
-                    }
-                }
-                
-                
-                //Message Text Input
-                ZStack(alignment: .trailing){
-                    Button {
-                        print("Send Message")
-                    } label: {
-                        Image(systemName: "paperplane.fill")
-                            .foregroundColor(Color("Accent"))
-                    }
-                    .padding(.horizontal)
+        VStack {
+            ScrollView{
+                VStack{
+                    Image(conversation.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 120)
+                        .mask(Circle())
+                        .foregroundColor(Color(.systemGray4))
+                        .padding(.vertical)
                     
-                    TextField("Message...", text: $messageText)
-                        .padding(12)
-                        .padding(.trailing, 48)
-                        .background(Color(.systemGroupedBackground))
-                        .clipShape(Capsule())
-                        .font(.subheadline)
+                    Text(conversation.members[1])
+                        .font(.title3)
+                        .fontWeight(.semibold)
                     
-                    Button {
-                        print("Send Message")
-                    } label: {
-                        Image(systemName: "paperplane.fill")
-                            .foregroundColor(Color("Accent"))
-                    }
-                    .padding(.horizontal)
-
+                    Text("Technicien")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .padding(.bottom)
+                    
                 }
-                .padding()
             }
-        }
-            .navigationBarTitle(conversation.isGroup ? conversation.name : conversation.members[1], displayMode: .inline)
-            .navigationBarItems(
-                leading: Image(conversation.image)
-                    .resizable()
-                    .scaledToFill()
-                    .mask(Circle())
-                    .frame(width: 30, height: 30),
-                trailing: Image(systemName: "info.circle")
+                .navigationBarTitle(conversation.isGroup ? conversation.name : conversation.members[1], displayMode: .inline)
+                .navigationBarItems(
+                    leading: Image(conversation.image)
+                        .resizable()
+                        .scaledToFill()
+                        .mask(Circle())
+                        .frame(width: 30, height: 30),
+                    trailing: Image(systemName: "info.circle")
                     .foregroundColor(Color("Accent")))
+            
+            //Message Text Input
+            ZStack{
+                HStack{
+                    PhotosPicker(selection: $viewModel.selectedItem){
+                        if let selectedImage = viewModel.selectedImage {
+                            selectedImage
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 180, height: 180)
+                                .clipShape(Rectangle())
+                        } else {
+                            Image(systemName: "photo.circle.fill")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                                .foregroundColor(Color("Accent"))
+                        }
+                    }
+                    HStack{
+                        TextField("Message...", text: $messageText, axis: .vertical)
+                            .padding(.vertical,12)
+                            .padding(.leading,12)
+                            .font(.subheadline)
+                        
+                        Button {
+                            messageText = ""
+                        } label: {
+                            Image(systemName: "paperplane.fill")
+                                .foregroundColor(Color("Accent"))
+                        }
+                        .padding(.trailing)
+                    }
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                }
+
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
